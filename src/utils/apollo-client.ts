ec4ad7f5ca936cb233cf27edab8d1ab330fb9133
Motahard <mas280499@gmail.com>
2024-07-21 20:17:21 +0300

    feat: refactoring api

--- src/utils/apollo-client.ts
+++ src/utils/apollo-client.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 export const createApolloClient = () => {
   console.log(process.env);
   return new ApolloClient({
-    uri: process.env.NEXT_PUBLIC_VERCEL_URL ? process.env.NEXT_PUBLIC_VERCEL_URL + '/api/graphql' : process.env.NEXT_PUBLIC_SERVER_GRAPHQL || 'localhost:3000/api/graphql',
+    uri: process.env.NEXT_PUBLIC_VERCEL_URL ? process.env.NEXT_PUBLIC_VERCEL_URL + '/api' : process.env.NEXT_PUBLIC_SERVER_GRAPHQL || 'localhost:3000/api',
     cache: new InMemoryCache(),
   });
 };
